8cafe61a25017d76082482a101b66aa2cc2ba275
matakawa <s.matakawa@so-ra.co.jp>
2017-06-13 11:44:08 +0900

    branchコミット１

--- hello.php
+++ hello.php
@@ -1,8 +1,8 @@
 <html>
  <head>
-  <title>PHP Test</title>
+  <title>PHP branch Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php echo '<p>Hello branch World</p>'; ?> 
  </body>
 </html>
