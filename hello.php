fcb3c1ad7548f6957f415d7325107fafd730d502
matakawa <s.matakawa@so-ra.co.jp>
2017-06-13 11:48:36 +0900

    branch2-1

--- hello.php
+++ hello.php
@@ -1,8 +1,8 @@
 <html>
  <head>
-  <title>PHP Test</title>
+  <title>PHP branch2 Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php echo '<p>Hello branch2 World</p>'; ?> 
  </body>
 </html>
